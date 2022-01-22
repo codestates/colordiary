c4fc8aaa2cb0c4fe88fcc81b5b4dc2d3f6a0480b
park_so_young <soyoung931014@gmail.com>
2022-01-23 00:27:13 +0900

    route

--- server/routes/links.js
+++ server/routes/links.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('../controllers');
+// const express = require('express');
+// const router = express.Router();
+// const controllers = require('../controllers');
 
-router.get('/user_accesstoken', controllers.user_accesstoken);
-router.get('/user_refreshtoken', controllers.user_refreshtoken);
-router.get('/user_mycolor', controllers.user_mycolor);
+// router.get('/user_accesstoken', controllers.user_accesstoken);
 
-router.post('/signup', controllers.signup);
-router.post('/login', controllers.login);
-router.post('/user_mycolor', controllers.user_mycolor);
-router.post('/user_logout', controllers.user_logout);
+// router.get('/user_refreshtoken', controllers.user_refreshtoken);
 
-router.put('/user_mycolor', controllers.user_mycolor);
+// router.get('/user_mycolor', controllers.user_mycolor);
+// router.post('/user_mycolor', controllers.user_mycolor);
+// router.put('/user_mycolor', controllers.user_mycolor);
+// router.delete('/user_mycolor', controllers.user_mycolor);
 
-router.delete('/user_mycolor', controllers.user_mycolor);
-router.delete('/user_delete', controllers.user_delete);
+// router.post('/signup', controllers.signup);
 
-module.exports = router;
+// router.post('/login', controllers.login);
+
+// router.post('/user_logout', controllers.user_logout);
+
+// router.delete('/user_delete', controllers.user_delete);
+
+// module.exports = router;
