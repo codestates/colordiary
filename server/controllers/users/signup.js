b94912bc85f058c8606661d6fae22cf9729e2469
Boyoung <boyoung.y589@gmail.com>
2022-01-23 16:34:23 +0900

    MERGE: router

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const { userInfo } = require('../../models');
 const { generateAccessToken, sendAccessToken } = require('../tokenfunction/token');
-=======
-// const { userinfo } = require('../../models');
-// const { generateAccessToken, sendAccessToken } = require('../tokenfunction/token');
->>>>>>> 7788729dddf12257d93bd34562b8e2f481cb14fe
 
 module.exports =  (req, res) => {
 
