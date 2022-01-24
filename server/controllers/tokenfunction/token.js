f30ee6d82a055d3ebe3902cfa001ac059813831e
park_so_young <soyoung931014@gmail.com>
2022-01-24 23:13:26 +0900

    ADD: login.js

--- server/controllers/tokenfunction/token.js
+++ server/controllers/tokenfunction/token.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 const { sign, verify } = require('jsonwebtoken');
-const accessKey = process.env.ACCESS_SECRET;
+const tokenKey = process.env.ACCESS_SECRET;
 const refreshKey = process.env.REFRESH_SECRET;
 
 module.exports = {
-    generateAccessToken: (data) => {
+    
         // const {username, email, mobile } = data //비밀번호 여기서 제외시켜주자
         // 어세스토큰발급, 리프레시토큰발급,
-
+    generateToken: (data, tokenKey, time) => {
+    
+        // 어세스토큰발급, 리프레시토큰발급,
+        const {email, username, mobile, createdAt, updatedAt} = data.dataValues;
+        const tokenMaker = sign({
+            email, username, mobile, createdAt, updatedAt
+        }, tokenKey, { 
+            algorithm: 'HS256', 
+            expiresIn: time
+        });
+        return tokenMaker;
     },
+
+
 
     sendAccessToken: (res, refreshtoken) => {
         
--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,8 +1,20 @@
-//const { userinfo } = require('../../models');
-//const { generateAccessToken, sendAccessToken } = require('../tokenfunction/token');
+const { userInfo } = require('../../models');
+const {generateToken, sendAccessToken } = require('../tokenfunction/token');
 
-module.exports = (req, res) => {
-
-    res.status(500).json({message: 'login'})
+module.exports = async (req, res) => {
+// 만약 req.body에 들어온값을 디비에서 찾아봐서 있으면 로그인 (어세스 리프레시 그리고 데이터에 유저인포보내줌)
+// 그치만 없으면 오류보냄 
+// req.body.password
+// req.body.email
+    const user = await userInfo.findOne({where: {email:'soyoung93104@gmail.com', password: 's1o2y3n4n6hg'}})
+    console.log(user)
+    if (!user) {
+        res.status(412).json({message: "로그인에 실패했습니다.", data : null})
+    } else {
+       const token = generateToken(user) // 비밀번호 넣어줌 토큰 펑션에서 없애주나?
+       //sendAccessToken(res,token)  // 반환값을 accesstoken     
+       res.status(200).json({message:"로그인에 성공했습니다.", data: token})
+    }
+    /* res.status(500).json({message: 'login'}) */
 
 }
