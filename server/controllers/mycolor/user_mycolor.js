06eeb83df94d719c3a43859343a0437aa004048b
park_so_young <soyoung931014@gmail.com>
2022-01-25 14:56:41 +0900

    dd

--- server/controllers/mycolor/user_mycolor.js
+++ server/controllers/mycolor/user_mycolor.js
@@ -5,12 +5,11 @@ const axios = require('axios')
 
 module.exports = async (req, res) => {
  
-  /*   
-    axios.get('"https://dapi.kakao.com/v2/search/web?sort=accuracy&page=1&size=1&query=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0"',
-    { header: { "Accept": "application/json","Authorization": "KakaoAK 6d89912785d7f47c3d841bc69527f77a",'Content-Type': 'application/json'}}
-    )
-    .then(result => console.log(result))  */
   
+//     await axios.get('https://dapi.kakao.com/v2/search/web?sort=accuracy&page=1&size=1&query=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+//     { headers: { "Accept": "*","Authorization": "KakaoAK 6d89912785d7f47c3d841bc69527f77a",'Content-Type': 'application/json'}})
+//     .then(result => console.log(result))  
+// } 
 
   /*   await axios.get("https://dapi.kakao.com/v3/search/book?sort=accuracy&page=1&target=title&query=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84%EC%9A%A9%EA%B8%B0&size=1",
     { headers: { "Authorization": '6d89912785d7f47c3d841bc69527f77a'}}
@@ -25,9 +24,16 @@ module.exports = async (req, res) => {
     )
     .then(result => console.log(result))  */
 
-   /*  fetch("https://nl.go.kr/NL/search/openApi/saseoApi.do?key=0af311ab5160ba5f6f530b5230e8784d7bbf09a7678a720e48a815a1c585d623&startRowNumApi=1&endRowNumApi=3&start_date=20190101&end_date=20200131&drcode=11")
-    .then(res => res.json())
-    .then(data => console.log(data)) */
+//     axios.get("https://nl.go.kr/NL/search/openApi/saseoApi.do?key=0af311ab5160ba5f6f530b5230e8784d7bbf09a7678a720e48a815a1c585d623&startRowNumApi=1&endRowNumApi=3&start_date=20190101&end_date=20200131&drcode=11&json",
+//     { header: { "Accept": "application/json"}})
+//     .then(result => {
+//       const{data}= result
+//       for (output in data){
+//         console.log("노드 값: "+JSON.stringify(output))
+// }
+
+//     })
+   
 
   //res.send("🐳")
    
