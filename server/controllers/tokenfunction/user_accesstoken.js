1ad0215b8b83ed5e384cc87ceb47abb6892ff45b
Boyoung <boyoung.y589@gmail.com>
2022-01-26 00:02:09 +0900

    FIX: Typo

--- server/controllers/tokenfunction/user_accesstoken.js
+++ server/controllers/tokenfunction/user_accesstoken.js
@@ -4,7 +4,7 @@ const { userinfo } = require('../../models');
 const accessKey = process.env.ACCESS_SECRET;
 const { isAuthorized } = require('./token')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     /*
     헤더 authorization 부분이 없는 요청 혹은 잘못된 토큰을 전달받은 경우, 응답에는 'invalid access token' 메세지가 포함되어야 합니다
     헤더 authorization 부분에 jwt 토큰이 존재하며 토큰에 유저정보가 담겨져 있는경우, 해당 유저의 정보를 리턴해야 합니다 
