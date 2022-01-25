3320be3e90af97e4c9cdbe6640a7eeb62e5dcbca
gardenia1109 <gardenia1109@naver.com>
2022-01-25 22:56:03 +0900

    UPDATE

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
