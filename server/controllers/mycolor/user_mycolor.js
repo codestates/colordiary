5acb8f4d40695deee1f3b12f81fafeb8ae86b3bf
park_so_young <soyoung931014@gmail.com>
2022-01-25 14:11:48 +0900

    dd

--- server/controllers/mycolor/user_mycolor.js
+++ server/controllers/mycolor/user_mycolor.js
@@ -1,8 +1,36 @@
 // const {color} = require('../../models');
 // const {contents} = require('../../models');
 // const {music} = require('../../models');
+const axios = require('axios')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+ 
+  /*   
+    axios.get('"https://dapi.kakao.com/v2/search/web?sort=accuracy&page=1&size=1&query=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0"',
+    { header: { "Accept": "application/json","Authorization": "KakaoAK 6d89912785d7f47c3d841bc69527f77a",'Content-Type': 'application/json'}}
+    )
+    .then(result => console.log(result))  */
+  
+
+  /*   await axios.get("https://dapi.kakao.com/v3/search/book?sort=accuracy&page=1&target=title&query=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84%EC%9A%A9%EA%B8%B0&size=1",
+    { headers: { "Authorization": '6d89912785d7f47c3d841bc69527f77a'}}
+    )
+    .then(result => console.log(result))  */
+
+
+
+
+ /*    await axios.get("https://dapi.kakao.com/v3/search/book?sort=accuracy&page=1&target=title&query=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84%EC%9A%A9%EA%B8%B0&size=1",
+    { headers: { "Authorization": '6d89912785d7f47c3d841bc69527f77a'}}
+    )
+    .then(result => console.log(result))  */
+
+   /*  fetch("https://nl.go.kr/NL/search/openApi/saseoApi.do?key=0af311ab5160ba5f6f530b5230e8784d7bbf09a7678a720e48a815a1c585d623&startRowNumApi=1&endRowNumApi=3&start_date=20190101&end_date=20200131&drcode=11")
+    .then(res => res.json())
+    .then(data => console.log(data)) */
+
   //res.send("🐳")
-   res.status(200).json({message: 'color'}) 
+   
+  //res.status(200).json({message: 'color'}) 
+
 }
