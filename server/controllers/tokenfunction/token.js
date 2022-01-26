c98606b51d1f51e9ba97bcec33b5ef409f307ef0
Boyoung <boyoung.y589@gmail.com>
2022-01-26 15:54:53 +0900

    FIX: request location

--- server/controllers/tokenfunction/token.js
+++ server/controllers/tokenfunction/token.js
@@ -30,29 +30,39 @@ module.exports = {
 
     isAuthorized: (req, tokenKey, time) => {
 
-        // 검증
-        //jwt.verify(token, secretOrPublicKey, [options, callback])
+        /*
+        검증
+        jwt.verify(token, secretOrPublicKey, [options, callback])
         console.log("리퀘스트는 뭐가 들어오나요?",req);
-        console.log("헤더 안에 들은건가요??",req.headers);
-        console.log("쿠키 안에 들은건가요???",req.cookie);
-        return;
-        // const headerAuth = req.headers[];
 
-        // if(!headerAuth){
-        //     return null;
-        // } else{
-        //     const tokenVerifier = verify(headerAuth, tokenKey, {
-        //         algorithm: 'HS256',
-        //         expiresIn: time
-        //     }, (err, decoded) => {
-        //         if(err){
-        //             console.log("토큰 검증과정 중 에러발생", err)
-        //         } else{
-        //             return decoded;
-        //         }
-        //     });
-        //     return tokenVerifier;
-        // }
+        authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+        eyJlbWFpbCI6ImYiLCJ1c2VybmFtZSI6ImYiLCJtb2JpbGUiOiJmIiwiaW
+        F0IjoxNjQzMTM5MDgxLCJleHAiOjE2NDMxOTkwODF9.6W9_tKWxgi-fmck
+        kE9wN615fkPG9JZFEtKSTEsBrpwY
+        */
+        console.log("헤더 안에 들은건가요??",req.headers);
+        console.log("바디에는 어떤 정보들이 오는건가요???",req.body);
+
+        const headerAuth = req.headers['authorization'];
+
+        console.log("클라이언트가 Bearer 토큰을 잘 보내줬나요??", headerAuth);
+
+        if(!headerAuth){
+            return null;
+        } else{
+            const auth = headerAuth.split(' ')[1];
+            const tokenVerifier = verify(auth, tokenKey, {
+                algorithm: 'HS256',
+                expiresIn: time
+            }, (err, decoded) => {
+                if(err){
+                    console.log("토큰 검증과정 중 에러발생", err)
+                } else{
+                    return decoded;
+                }
+            });
+            return tokenVerifier;
+        }
         
     }
 }
