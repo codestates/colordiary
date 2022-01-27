4cb3c467c5db670e7c03dcc6ed005ea14f6f3ad3
park_so_young <soyoung931014@gmail.com>
2022-01-28 07:39:11 +0900

    dw

--- server/migrations/03-create-contents.js
+++ server/migrations/03-create-contents.js
@@ -1,34 +1,17 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-<<<<<<< HEAD:server/migrations/20220126083830-create-user-info.js
-    await queryInterface.createTable('userInfos', {
-=======
     await queryInterface.createTable('contents', {
->>>>>>> 9342f6fafe461d35d15d36f256172d1238bdfaf3:server/migrations/03-create-contents.js
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-<<<<<<< HEAD:server/migrations/20220126083830-create-user-info.js
-      username: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      mobile: {
-=======
       message: {
         type: Sequelize.STRING
       },
       moodDate: {
->>>>>>> 9342f6fafe461d35d15d36f256172d1238bdfaf3:server/migrations/03-create-contents.js
         type: Sequelize.STRING
       },
       createdAt: {
@@ -42,10 +25,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-<<<<<<< HEAD:server/migrations/20220126083830-create-user-info.js
-    await queryInterface.dropTable('userInfos');
-=======
     await queryInterface.dropTable('contents');
->>>>>>> 9342f6fafe461d35d15d36f256172d1238bdfaf3:server/migrations/03-create-contents.js
   }
 };
